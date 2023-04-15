be20055f6d21cf7e5c12e503993faf225d0644d7
KhayitovDev <khayitovkhayitboy2109@gmail.com>
2023-04-15 12:09:43 +0500

    conf models

--- task/models.py
+++ task/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Menu(models.Model):
+    title=models.CharField(max_length=250, blank=True)
+    url=models.CharField(max_length=250)
+    named_url=models.CharField(max_length=250, blank=True)
+    parent=models.ForeignKey('self', on_delete=models.CASCADE,  null=True, blank=True)
+    is_active=models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return self.title
+    
